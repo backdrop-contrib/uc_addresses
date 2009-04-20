5bce69cbc91bc2bb2ceb1534fbbfa1ff07af43dd
Tony Freixas <tony@tigerheron.com>
2009-04-20 20:50:00 +0000

    ucCountrySelect changed to ucAddressesCountrySelect. Added form build ID as a POST parameter. Now calls uc_addresses_js_util instead of uc_js_util.

--- uc_addresses.js
+++ uc_addresses.js
@@ -3,31 +3,40 @@
 /**
  * Set the select box change behavior for the country selector
  */
-Drupal.behaviors.ucAddressCountrySelect = function(context) {
-  $('select[@id*=-country-]:not(.ucCountrySelect-processed)', context).addClass('ucCountrySelect-processed').change(
-    function() {
-      uc_addresses_update_zone_select(this.id, '');
-    }
-  );
+Drupal.behaviors.ucAddressesCountrySelect = function(context) {
+  $('select[@id*=-country-]:not(.ucAddressesCountrySelect-processed)', context).addClass('ucAddressesCountrySelect-processed').change(
+      function() {
+	uc_addresses_update_zone_select(this.id, '');
+      }
+    );
 }
+
 function uc_addresses_update_zone_select(country_select, default_zone) {
-  var zone_select = country_select.substr(0, country_select.length - 10) + '-zone' +
-      country_select.substr(country_select.length -2 );
+  var zone_select =
+    country_select.substr(0, country_select.length - 10) +
+    '-zone' +
+    country_select.substr(country_select.length - 2 );
 
-  var options = { 'country_id' : $('#' + country_select).val() };
+  var options = {
+    'country_id' : $('#' + country_select).val(),
+    'form_build_id' : $('form[@id^=uc-addresses] input[@name=form_build_id]').val()
+  };
 
-  $('#' + zone_select).parent().siblings('.zone-throbber').attr('style', 'background-image: url(' + Drupal.settings.basePath + 'misc/throbber.gif); background-repeat: no-repeat; background-position: 100% -20px;').html('&nbsp;&nbsp;&nbsp;&nbsp;');
+  $('#' + zone_select).parent()
+    .siblings('.zone-throbber')
+    .attr('style', 'background-image: url(' + Drupal.settings.basePath + 'misc/throbber.gif); background-repeat: no-repeat; background-position: 100% -20px;')
+    .html('&nbsp;&nbsp;&nbsp;&nbsp;');
 
-  $.post(Drupal.settings.basePath + '?q=uc_js_util/zone_select', options,
-         function (contents) {
-           if (contents.match('value="-1"') != null) {
-             $('#' + zone_select).attr('disabled', 'disabled');
-           }
-           else {
-             $('#' + zone_select).removeAttr('disabled');
-           }
-           $('#' + zone_select).empty().append(contents).val(default_zone).change();
-           $('#' + zone_select).parent().siblings('.zone-throbber').removeAttr('style').empty();
-         }
-  );
+  $.post(Drupal.settings.basePath + '?q=uc_addresses_js_util', options,
+	 function (contents) {
+	   if (contents.match('value="-1"') != null) {
+	     $('#' + zone_select).attr('disabled', 'disabled');
+	   }
+	   else {
+	     $('#' + zone_select).removeAttr('disabled');
+	   }
+	   $('#' + zone_select).empty().append(contents).val(default_zone).change();
+	   $('#' + zone_select).parent().siblings('.zone-throbber').removeAttr('style').empty();
+	 }
+    );
 }
