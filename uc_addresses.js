27ae434535172b17fec6a10ff9624f8bc8fc50a4
Tony Freixas <tony@tigerheron.com>
2009-12-14 17:47:23 +0000

    Bug #376632 by tic2000: Fixed an error that occurred when changing the zone value during checkout.

--- uc_addresses.js
+++ uc_addresses.js
@@ -4,39 +4,12 @@
  * Set the select box change behavior for the country selector
  */
 Drupal.behaviors.ucAddressesCountrySelect = function(context) {
-  $('select[id*=-country-]:not(.ucAddressesCountrySelect-processed)', context).addClass('ucAddressesCountrySelect-processed').change(
+  if (Drupal.settings.uc_address_default) {
+    $('select[id$=-country]:not(.ucAddressCountrySelect-processed)', context).each(
       function() {
-	uc_addresses_update_zone_select(this.id, '');
+        uc_update_zone_select(this.id, Drupal.settings.uc_address_default.zone);
+        $(this).addClass('ucAddressCountrySelect-processed');
       }
     );
-}
-
-function uc_addresses_update_zone_select(country_select, default_zone) {
-  var zone_select =
-    country_select.substr(0, country_select.length - 10) +
-    '-zone' +
-    country_select.substr(country_select.length - 2 );
-
-  var options = {
-    'country_id' : $('#' + country_select).val(),
-    'form_build_id' : $('form[id^=uc-addresses] input[name=form_build_id]').val()
-  };
-
-  $('#' + zone_select).parent()
-    .siblings('.zone-throbber')
-    .attr('style', 'background-image: url(' + Drupal.settings.basePath + 'misc/throbber.gif); background-repeat: no-repeat; background-position: 100% -20px;')
-    .html('&nbsp;&nbsp;&nbsp;&nbsp;');
-
-  $.post(Drupal.settings.basePath + '?q=uc_addresses_js_util', options,
-	 function (contents) {
-	   if (contents.match('value="-1"') != null) {
-	     $('#' + zone_select).attr('disabled', 'disabled');
-	   }
-	   else {
-	     $('#' + zone_select).removeAttr('disabled');
-	   }
-	   $('#' + zone_select).empty().append(contents).val(default_zone).change();
-	   $('#' + zone_select).parent().siblings('.zone-throbber').removeAttr('style').empty();
-	 }
-    );
+  }
 }
