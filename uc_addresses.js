7b63c970b9b3a5b6e354b21b38c90e4ca3502618
Tony Freixas <tony@tigerheron.com>
2009-12-17 01:23:51 +0000

    Bug #376632 by freixas: Fixed: removed unneeded class setting.

--- uc_addresses.js
+++ uc_addresses.js
@@ -8,7 +8,6 @@ Drupal.behaviors.ucAddressesCountrySelect = function(context) {
     $('select[id$=-country]:not(.ucAddressCountrySelect-processed)', context).each(
       function() {
         uc_update_zone_select(this.id, Drupal.settings.uc_address_default.zone);
-        $(this).addClass('ucAddressCountrySelect-processed');
       }
     );
   }
