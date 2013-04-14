2820d907e2d18325ee84a6f28f9ec06bdb10045d
MegaChriz <megachriz@hotmail.com>
2013-04-14 16:16:54 +0200

    Issue #1433034 by MegaChriz: added default View for the address book page.

--- templates/uc-addresses-list-address.tpl.php
+++ templates/uc-addresses-list-address.tpl.php
@@ -41,30 +41,32 @@
  * @ingroup themeable
  */
 ?>
-<?php if ($label): ?>
-  <h3><?php print $label; ?></h3>
-<?php endif; ?>
-<table class="list-address <?php print $classes; ?>">
-  <tbody>
-    <?php if (is_array($fields) && count($fields) > 0): ?>
-      <?php foreach ($fields as $field_name => $field): ?>
-        <tr class="data-row address-field-<?php print $field_name; ?>">
-          <td class="title-col">
-            <?php if ($field['title'] != ''):
-              print $field['title'] . ':';
-            endif; ?>
-          </td>
-          <td class="data-col"><?php print $field['data']; ?></td>
-        </tr>
-      <?php endforeach; ?>
-    <?php endif; ?>
+<div class="list-address-wrapper">
+  <?php if ($label): ?>
+    <h3><?php print $label; ?></h3>
+  <?php endif; ?>
+  <table class="list-address <?php print $classes; ?>">
+    <tbody>
+      <?php if (is_array($fields) && count($fields) > 0): ?>
+        <?php foreach ($fields as $field_name => $field): ?>
+          <tr class="data-row address-field-<?php print $field_name; ?>">
+            <td class="title-col">
+              <?php if ($field['title'] != ''):
+                print $field['title'] . ':';
+              endif; ?>
+            </td>
+            <td class="data-col"><?php print $field['data']; ?></td>
+          </tr>
+        <?php endforeach; ?>
+      <?php endif; ?>
 
-    <?php if ($admin_links): ?>
-      <tr class="address-links">
-        <td colspan="2">
-          <?php print $admin_links; ?>
-        </td>
-      </tr>
-    <?php endif; ?>
-  </tbody>
-</table>
+      <?php if ($admin_links): ?>
+        <tr class="address-links">
+          <td colspan="2">
+            <?php print $admin_links; ?>
+          </td>
+        </tr>
+      <?php endif; ?>
+    </tbody>
+  </table>
+</div>
